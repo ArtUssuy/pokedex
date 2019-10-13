5232fa3da0514e4790d83f32727f91852d37732b
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-10-13 01:45:36 -0300

    implementada paginacao

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,52 +1,93 @@
-import React, { useState, useEffect, setState } from 'react'
-import axios from 'axios'
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import InfiniteScroll from "react-infinite-scroll-component";
+import ReactPaginate from "react-paginate";
 
-import { Container } from './styles'
-import Card from './../cards/cards'
-
+import { Container } from "./styles";
+import Card from "./../cards/cards";
 
 const Board = () => {
+	const [data, setData] = useState([]);
+	const [limit, setLimit] = useState(100);
+	const [set, setSet] = useState(0);
 
-    const [data, setData] = useState([])
-    const [times, setTimes] = useState(100)
+	useEffect(() => {
+		console.log("TCL: Board -> useEffect");
+		fetchData();
+	}, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${times}`
-            const response = await axios(url)
-            setData(response.data.results)
-        }
-        fetchData()
-    })
+	const fetchData = async () => {
+		const url = `https://pokeapi.co/api/v2/pokemon?offset=${set}&limit=${limit}`;
+		const response = await axios(url);
+		setData(response.data.results);
+	};
 
-    const fetchMoreData = () => {
-		let number = times 
-		let number2 = number + 100
-		setTimes(number2)
-    }
+	const fetchMoreData = () => {
+		// eslint-disable-next-line no-const-assign
+		setLimit((limit += 100));
+	};
 
-  return (
-        <InfiniteScroll
-          dataLength={data.length}
-          next={fetchMoreData}
-          hasMore={times < 1090 ? true : false}
-		  endMessage={"End!"}
-          loader={<h4>Loading...</h4>}
-        >
-            <Container>
-            {
-                data !== undefined ? data.map((pokemon) => 
-                    <Card 
-                        key={pokemon.name} 
-                        pokemonName={pokemon.name} 
-                        pokemonIndex={pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
-                    />) : ""
-            }
-            </Container>
+	const handlePageClick = e => {
+		setSet(`${e.selected}00`);
+		console.log("TCL: Board -> set", set);
+		setLimit(`${e.selected + 1}00`);
+		console.log("TCL: Board -> limit", limit);
+		fetchData();
+	};
 
-        </InfiniteScroll>
-  );
-}
+	return (
+		<>
+			<Container>
+				{data !== undefined
+					? data.map(pokemon => (
+							<Card
+								key={pokemon.name}
+								pokemonName={pokemon.name}
+								pokemonIndex={
+									pokemon.url.split("/")[
+										pokemon.url.split("/").length - 2
+									]
+								}
+							/>
+					  ))
+					: ""}
+			</Container>
 
-export default Board
+			<ReactPaginate
+				previousLabel={"previous"}
+				nextLabel={"next"}
+				breakLabel={"..."}
+				breakClassName={"break-me"}
+				pageCount={11}
+				marginPagesDisplayed={2}
+				pageRangeDisplayed={5}
+				onPageChange={handlePageClick}
+				containerClassName={"pagination"}
+				subContainerClassName={"pages pagination"}
+				activeClassName={"active"}
+			/>
+		</>
+
+		// <InfiniteScroll
+		//   dataLength={data.length}
+		//   next={fetchMoreData}
+		//   hasMore={limit < 1090 ? true : false}
+		//   endMessage={"End!"}
+		//   loader={<h4>Loading...</h4>}
+		// >
+		//     <Container>
+		//     {
+		//         data !== undefined ? data.map((pokemon) =>
+		//             <Card
+		//                 key={pokemon.name}
+		//                 pokemonName={pokemon.name}
+		//                 pokemonIndex={pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
+		//             />) : ""
+		//     }
+		//     </Container>
+
+		// </InfiniteScroll>
+	);
+};
+
+export default Board;
