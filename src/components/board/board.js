e900f4503bf55a726b9d9fc3e10b2e65c7f1cb78
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-10-12 21:26:04 -0300

    infinity scroll

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, setState } from 'react'
 import axios from 'axios'
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import { Container } from './styles'
 import Card from './../cards/cards'
@@ -8,31 +8,44 @@ import Card from './../cards/cards'
 
 const Board = () => {
 
-    const [data, setData] = useState()
+    const [data, setData] = useState([])
+    const [times, setTimes] = useState(100)
 
     useEffect(() => {
         const fetchData = async () => {
-            const url = "https://pokeapi.co/api/v2/pokemon"
+            const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${times}`
             const response = await axios(url)
             setData(response.data.results)
         }
-        
         fetchData()
-        
+    })
 
-    }, [])
+    const fetchMoreData = () => {
+		let number = times 
+		let number2 = number + 100
+		setTimes(number2)
+    }
 
   return (
-    <Container>
-        {
-            data !== undefined ? data.map((pokemon) => 
-                <Card 
-                    key={pokemon.name} 
-                    pokemonName={pokemon.name} 
-                    pokemonIndex={pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
-                />) : ""
-        }
-    </Container>
+        <InfiniteScroll
+          dataLength={data.length}
+          next={fetchMoreData}
+          hasMore={times < 200 ? true : false}
+		  endMessage={"End!"}
+          loader={<h4>Loading...</h4>}
+        >
+            <Container>
+            {
+                data !== undefined ? data.map((pokemon) => 
+                    <Card 
+                        key={pokemon.name} 
+                        pokemonName={pokemon.name} 
+                        pokemonIndex={pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
+                    />) : ""
+            }
+            </Container>
+
+        </InfiniteScroll>
   );
 }
 
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Image, Label } from './styles';
 
 const Card = ({pokemonName, pokemonIndex}) => {
